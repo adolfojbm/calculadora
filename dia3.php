52b6f21e326bb5fb6f6d0f289eb0895a74160735
adolfojbm <adolfojm2020@gmail.com>
2021-04-04 23:33:46 +0200

    calculadora5

--- dia3.php
+++ dia3.php
@@ -10,32 +10,34 @@
 	<div class="card-body">
 	    <h5 class="card-title">Calculadora</h5>
 	    <div class="row">
-	    	<div class="col-12">
+	    	<div class="col-12"style="padding:0">
 		    	<input type="text" name="texto" value="0" style="width: 100%;">
+		    	<input type="text" name="texto1" value="0" style="width: 100%;">
+		    	<input type="" name="siete" value="7">
 		    </div>
-		    <div class="col-3">										
-		    	<input type="button" name="siete" value="7" style="width: 100%;">
-		    	<input type="button" name="cuatro" value="4" style="width: 100%;">
-		    	<input type="button" name="uno" value="1" style="width: 100%;">
-		    	<input type="button" name="cero" value="0" style="width: 100%;">
+		    <div class="col-3" style="padding:0">										
+		    	<input id="siete"  type="button" name="siete" value="7" style="width: 100%;">
+		    	<input id="cuatro"  type="button" name="cuatro" value="4" style="width: 100%;">
+		    	<input id="uno"  type="button" name="uno" value="1" style="width: 100%;">
+		    	<input id="cero"  type="button" name="cero" value="0" style="width: 100%;">
 		    </div>
-		    <div class="col-3">
-		    	<input type="button" name="ocho" value="8" style="width: 100%;">
-		    	<input type="button" name="cinco" value="5" style="width: 100%;">
-		    	<input type="button" name="dos" value="2" style="width: 100%;">
-		    	<input type="button" name="punto" value="." style="width: 100%;">
+		    <div class="col-3" style="padding:0">
+		    	<input id="ocho"  type="button" name="ocho" value="8" style="width: 100%;">
+		    	<input id="cinco"  type="button" name="cinco" value="5" style="width: 100%;">
+		    	<input id="dos"  type="button" name="dos" value="2" style="width: 100%;">
+		    	<input id="punto"  type="button" name="punto" value="." style="width: 100%;">
 		    </div>
-		    <div class="col-3">
-		    	<input type="button" name="nueve" value="9" style="width: 100%;">
-		    	<input type="button" name="seis" value="6" style="width: 100%;">
-		    	<input type="button" name="tres" value="3" style="width: 100%;">
-		    	<input type="button" name="mas" value="+" style="width: 100%;">
+		    <div class="col-3" style="padding:0">
+		    	<input id="nueve"  type="button" name="nueve" value="9" style="width: 100%;">
+		    	<input id="seis"  type="button" name="seis" value="6" style="width: 100%;">
+		    	<input id="tres"  type="button" name="tres" value="3" style="width: 100%;">
+		    	<input id="mas"  type="button" name="mas" value="+" style="width: 100%;">
 		    </div>		   
-		   	<div class="col-3">	
-		     <input type="button" name="dividir" value="/" style="width: 100%;">
-		     <input type="button" name="por" value="*" style="width: 100%;">
-		     <input type="button" name="menos" value="-" style="width: 100%;">		         
-		     <input type="button" name="igual" value="=" style="width: 100%;">
+		   	<div class="col-3" style="padding:0">	
+		     <input id="dividir" type="button" name="dividir" value="/" style="width: 100%;">
+		     <input id="por"type="button" name="por" value="*" style="width: 100%;">
+		     <input id="menos"type="button" name="menos" value="-" style="width: 100%;">		         
+		     <input id="igual"type="button" name="igual" value="=" style="width: 100%;">
 		    </div>	
 	    
 	    </div>
@@ -46,43 +48,36 @@
 <?php	
 		$A=0;
 		$B=0;	
+			
 
 	if (isset($_GET ["mas"]))
 	{
 		$A=$_GET["A"];
 		$B=$_GET["B"];
-		$C=$_GET["C"];
-		$D=$_GET["D"];
-		$F=$A+$B+$C+$D;
+		$F=$A+$B;
 		echo "EL RESULTADO ES ".$F;
 	}
 	if (isset($_GET ["menos"]))
 	{
 		$A=$_GET["A"];
 		$B=$_GET["B"];
-		$C=$_GET["C"];
-		$D=$_GET["D"];
-		$F=$A-$B-$C-$D;
+		$F=$A-$B;
 		echo "EL RESULTADO ES ".$F;
 	}
 	if (isset($_GET ["por"]))
 	{
 		$A=$_GET["A"];
 		$B=$_GET["B"];
-		$C=$_GET["C"];
-		$D=$_GET["D"];
-		$F=$A*$B*$C*$D;
+		$F=$A*$B;
 		echo "EL RESULTADO ES ".$F;
 	}
 	if (isset($_GET ["dividir"]))
 	{
 		$A=$_GET["A"];
 		$B=$_GET["B"];
-		$C=$_GET["C"];
-		$D=$_GET["D"];
-		$F=$A/$B/$C/$D;
+		$F=$A/$B;
 		echo "EL RESULTADO ES ".$F;
-
+	
 	}
 ?>
 
---
+++ dia3parte2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>calculadoraparte5</div>
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="practicadecalculadora3">
+		<div class="output">
+			<input class="display" type="text" id="result" placeholder="0">
+		</div>
+		<button name="data_number">1</button>			
+		<button name="data_number">2</button>					
+		<button name="data_number">3</button>					
+		<button name="data_opera">+</button>			
+		<button name="data_number">4</button>					
+		<button name="data_number">5</button>					
+		<button name="data_number">6</button>					
+		<button name="data_opera">-</button>				
+		<button name="data_number">7</button>			
+		<button name="data_number">8</button>		
+		<button name="data_number">9</button>
+		<button name="data_opera">*</button>	
+		<button name="data_number">0</button>
+		<button name="data_delete">C</button>
+		<button name="data_igual">=</button>
+		<button name="data_opera">/</button>
+	</div>
+    <script src="micodigo.js"></script>
+</body>
+</html>
